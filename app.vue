ae9812fc07a50b355ddc80d3656860a80b2b7fa8
novellac <38117965+novellac@users.noreply.github.com>
2025-03-29 19:30:55 -0400

    feat: Add global json for site strings

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { tinaField } from '@/composables/tinaField'
+import { useTina } from '@/composables/useTina'
+import { client } from '@/tina/__generated__/client'
+
+const siteResponse = await client.queries.site({ relativePath: `global.json` })
+
+const { data } = useTina({
+  query: siteResponse.query,
+  variables: siteResponse.variables,
+  data: siteResponse.data.site,
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
@@ -5,5 +19,12 @@
     <main>
       <NuxtPage />
     </main>
+    <footer class="bg-purple-100 p-4">
+      {{ data.siteName }}<br>
+      <div class="prose lg:prose-xl mt-2" :data-tina-field="tinaField(data, 'footerText')">
+        <TinaMarkdown :content="data?.footerText || []" />
+      </div>
+      <p>© {{ new Date().getFullYear() }} {{ data.siteCopyright }}</p>
+    </footer>
   </div>
 </template>
--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'tinacms'
 
+import { site } from './content-types/site'
+
 // Your hosting provider likely exposes this as an environment variable
 const branch
   = process.env.GITHUB_BRANCH
@@ -28,6 +30,7 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
+      site,
       {
         name: 'post',
         label: 'Posts',
